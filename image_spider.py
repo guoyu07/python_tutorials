e50232cb2ed6a0022023067783044c1a173b289e
Henry Lee <manageyp@gmail.com>
2013-11-22 13:17:26 +0800

    [Change] image spider

--- image_spider.py
+++ image_spider.py
@@ -1,12 +1,30 @@
 # -- coding: utf-8 --
 
+import re
+import urllib2
+
 class ImageSpider:
 
   @classmethod
   def fetch(cls):
     file = open("./doc/college_names.txt", 'r')
     for line in file:
-      print line
+      name = line.strip()
+      print "Search image for %s" %name
+
+      url = "http://cn.bing.com/images/search?q=%s&go=&qs=n&form=QBIR&pq=%s&sc=8-4&sp=-1&sk=" %(name, name)
+      html = urllib2.urlopen(url).read()
+      #print "Html result %s" %html
+
+      images = re.findall(r'<img(.*)>', html)
+      index = 0
+      for image in images:
+        if index < 3:
+          index += 1
+          print image
+        else:
+          break
+
     file.close()
 
 ImageSpider.fetch()
