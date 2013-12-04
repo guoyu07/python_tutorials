775623f94b0c364458ed62120958f0612472a668
Henry Lee <manageyp@gmail.com>
2013-12-04 11:25:37 +0800

    [NewFeature] module demo,tips

---
+++ module_demo.py
@@ -0,0 +1,4 @@
+# -- coding: utf-8 --
+
+def what_you_like(name='apple'):
+  print "You like %s." %name
---
+++ module_tips.py
@@ -0,0 +1,6 @@
+# -- coding: utf-8 --
+
+import module_demo
+
+module_demo.what_you_like()
+module_demo.what_you_like('Python')
