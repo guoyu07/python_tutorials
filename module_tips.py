ccdd4cfe2c1cc1197bb94c2731079a5ecc0195cd
Henry Lee <manageyp@gmail.com>
2013-12-04 17:31:01 +0800

    [Change] alternative import module

--- module_tips.py
+++ module_tips.py
@@ -1,6 +1,11 @@
 # -- coding: utf-8 --
 
-import module_demo
+# import module_demo
 
-module_demo.what_you_like()
-module_demo.what_you_like('Python')
+# module_demo.what_you_like()
+# module_demo.what_you_like('Python')
+
+from module_demo import what_you_like
+
+what_you_like()
+what_you_like('Python')
