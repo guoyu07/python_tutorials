b16aaddf78a1d792a49173a176043221880ebb77
Henry Lee <manageyp@gmail.com>
2014-06-07 13:10:57 +0800

    [Change] computing dict data to md5 hash

--- hash_tips.py
+++ hash_tips.py
@@ -69,6 +69,33 @@ s1 = dict.fromkeys(xrange(days))
 # >>> s1
 # {0: None, 1: None, 2: None}
 
-s2 = dict.fromkeys(xrange(days), [])
-# >>> s2
-# {0: [], 1: [], 2: []}
+# s2 = dict.fromkeys(xrange(days), [])
+# # >>> s2
+# # {0: [], 1: [], 2: []}
+# Bug list value are the same
+
+dash('create new dictionary with value set to an new array')
+days = 3
+value = []
+
+t = {day:list(value) for day in xrange(days)}
+print t
+
+t[0].append(0)
+t[1].append(1)
+print t
+#{0: [0], 1: [1], 2: []}
+
+dash('Computing an md5 hash of a dict data structure')
+import hashlib
+import json
+
+data1 = {'name': 'a', 'age': 2, 'tags': ['ruby', 1]}
+data1_md5 = hashlib.md5(json.dumps(data1, sort_keys=True)).hexdigest()
+print data1_md5
+
+data2 = {'age': 2, 'name': 'a', 'tags': ['ruby', 2]}
+data2_md5 = hashlib.md5(json.dumps(data2, sort_keys=True)).hexdigest()
+print data2_md5
+
+print data1_md5 == data2_md5
